d9f7b7d1cdb8a006565f703417612f0fad180d43
David Kovacs <david.kovacs@ngena.net>
2022-05-26 22:10:19 +0200

    sqlite created, basic get by id, gitignore adjusted

---
+++ source/app.py
@@ -0,0 +1,24 @@
+from enum import unique
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import requests
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data/data.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '{} : {}'.format(self.id, self.title)
+
+@app.route('/')
+def index():
+    return 'Hey!'
+
+@app.get('/posts/<id>')
+def get_post(id):
+    post = Post.query.get(id)
+    return {"title" : post.title}
