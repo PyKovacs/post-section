1739b2ee98a8f413a7f851a437155e1df0f27f4d
David Kovacs <david.kovacs@ngena.net>
2022-05-30 06:38:16 +0200

    added setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Post-Section',
+    version='0.1dev',
+    packages=['postsection',],
+    license='MIT license',
+    long_description=open('README.txt').read(),
+)
