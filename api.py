df6f596b824f285dd8af72bab00b63de1a8e8e13
David Kovacs <davi.kovacs.1991@gmail.com>
2023-03-14 14:51:56 +0100

    type annotations for api.py

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, Response
 
 from source.model import Post
 
@@ -6,26 +6,26 @@ api_interface = Blueprint(__name__, 'api_interface')
 post_model = Post()
 
 @api_interface.get('/posts/<post_id>')
-def get_post(post_id):
+def get_post(post_id) -> tuple[dict[str, (int|str)], int]:
     '''Get post based on post id.'''
     return Post.get(post_id)
 
 @api_interface.get('/posts/from_user=<user_id>')
-def get_user_posts(user_id):
+def get_user_posts(user_id) -> tuple[(Response | dict[str, (int|str)]), int]:
     '''Get posts based on user id.'''
     return Post.user_get(user_id)
 
 @api_interface.post('/posts')
-def add_post():
+def add_post() -> tuple[dict[str, (int|str)], int]:
     '''Add post.'''
     return Post.add()
 
 @api_interface.put('/posts/<post_id>')
-def update_post(post_id):
+def update_post(post_id) -> tuple[dict[str, dict[str, (int|str)]], int]:
     '''Update post.'''
     return Post.update(post_id)
 
 @api_interface.delete('/posts/<post_id>')
-def delete_post(post_id):
+def delete_post(post_id) -> tuple[dict[str, str], int]:
     '''Delete post.'''
     return Post.delete(post_id)
